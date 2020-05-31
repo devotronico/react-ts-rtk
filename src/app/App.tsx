7ed2ba86de127877b1100d208eced4e931cfe129
Daniele <dmanzi83@gmail.com>
2020-05-31 14:22:16 +0200

    first commit

---
+++ src/app/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+// import { Counter } from 'features/counter/Counter';
+import Todo from 'components/Todo';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <Counter /> */}
+        <Todo />
+      </header>
+    </div>
+  );
+}
+
+export default App;
